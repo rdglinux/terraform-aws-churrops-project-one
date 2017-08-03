a78e770ab5ec1d3960adcaab7447389747bb84ab
rdglinux <florianocomercial@gmail.com>
2017-08-02 22:49:33 -0300

    .
    .

--- nginx.sh
+++ nginx.sh
@@ -2,3 +2,4 @@
 yum update -y
 yum install nginx -y
 /etc/init.d/nginx start
+echo "<h1><strong> ChurrOps on DevOps </strong></h1><h3>Sucesso TOTAL</h3><br>AWS Instance created by Terraform<br>" > /usr/share/nginx/html/index.html
